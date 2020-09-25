2b311a701c3169e70cb6a7f5caff024156703af7
Ajayvarma1602 <ajayvarma1602@gmail.com>
2020-09-25 19:26:19 +0530

    Add details of tweetapp

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Tweet(models.Model):
-    content=models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    
+    content=models.CharField (max_length=150)
+    updated =models.DateTimeField(auto_now=True)
+    timestamp =models.DateTimeField( auto_now_add=True)
+
 
     def __str__(self):
         
